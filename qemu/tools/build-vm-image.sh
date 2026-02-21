6e25d684082622ea3b6339e2d9b9275badae4f0e
John Robbins <mulvanyrobbinsj@gmail.com>
2026-02-20 18:52:59 -0700

    Add script for building simplistic VM

---
+++ qemu/tools/build-vm-image.sh
@@ -0,0 +1,16 @@
+mkdir -p ./qemu-vms/images ./qemu-vms/vms/testvm
+pushd ./qemu-vms/images
+
+wget -O noble-amd64.img \
+  https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
+
+popd
+
+cd ./qemu-vms/vms/testvm
+
+qemu-img create -f qcow2 \
+  -F qcow2 \
+  -b ../../images/noble-amd64.img \
+  testvm.qcow2 40G
+
+qemu-img info testvm.qcow2
